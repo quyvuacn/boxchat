1318ff153e475563fe5bd79e523a8c12180e6d78
quyvuacn <87663363+quyvuacn@users.noreply.github.com>
2022-11-08 08:05:37 +0700

    quý

--- public/client.js
+++ public/client.js
@@ -1,43 +1,37 @@
 var socket = io.connect(window.location.href)
-    
-socket.on('connect', function (data) {
-    var name=$('#name').val()
-    var userID = $('#id').val()
-    var path = $('#path').val()
-    socket.emit('join',name)
-    sessionStorage.setItem("userID", userID)
 
-    $('form').submit(function(){
-        var message = $('#message').val();
-        socket.emit('messages',
-            {
-                id : userID,
-                name:name,
-                message:message,
-                path
-            }
-        )
-        this.reset()
-        return false
-    });
-});
+socket.on("connect", function (data) {
+	var name = prompt("Mời bạn nhập tên hiển thị") || "Guest"
+	socket.emit("join", name)
+	userID = socket.id
 
-
+	$("form").submit(function () {
+		var message = $("#message").val()
+		socket.emit("messages", {
+			id: userID,
+			name: name,
+			message: message,
+			path,
+		})
+		this.reset()
+		return false
+	})
+})
 
 //listen thread event
-socket.on('thread', function (data) {
-    if(data.id == sessionStorage.getItem("userID")) {
-        $('#thread').append(`
+socket.on("thread", function (data) {
+	if (data.id == sessionStorage.getItem("userID")) {
+		$("#thread").append(`
         <li class="myseft">
             <span>${data.message}</span>
         </li>
         `)
-    }else{
-        $('#thread').append(`
+	} else {
+		$("#thread").append(`
             <li>
                 <img src="${data.path}" alt="">
                 <span>${data.message}</span>
             </li>
         `)
-    }
+	}
 })
--- server.js
+++ server.js
@@ -1,68 +1,34 @@
-require('dotenv').config()
-var express = require('express');
-var app = express();
-var session = require('express-session');
-var passport = require('passport');
-var path = require('path')
+require("dotenv").config()
+var express = require("express")
+var app = express()
+var session = require("express-session")
+var path = require("path")
 var port = process.env.PORT || 8080
 
-var server = require('http').createServer(app);
+var server = require("http").createServer(app)
 
-var io = require('socket.io')(server);
-require('./auth');
+var io = require("socket.io")(server)
 //Public
-app.use(express.static('public'));
+app.use(express.static("public"))
 //View engine
-app.set('view engine', 'ejs');
-app.set('views',path.join(__dirname,'/views'))
+app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, "/views"))
 
-app.use(session({ secret: 'cats', resave: false, saveUninitialized: true }));
+app.use(session({ secret: "cats", resave: false, saveUninitialized: true }))
 
-app.use(passport.initialize());
-app.use(passport.session());
+app.get("/", function (req, res) {
+	res.render("index", { user: req.user })
+})
 
-app.get('/auth/google',
-  passport.authenticate('google', { scope: [ 'email', 'profile' ] }
-));
-
-app.get( '/auth/google/callback',
-  passport.authenticate( 'google', {
-    successRedirect: '/',
-    failureRedirect: '/auth/google/failure'
-  })
-);
-
-app.get('/logout', (req, res) => {
-    req.logout(req.user, err => {
-        if(err) return next(err);
-        res.redirect("/");
-      })
-    req.session.destroy();
-    res.send('Goodbye!');
-  });
-  
-  app.get('/auth/google/failure', (req, res) => {
-    res.send('Failed to authenticate..');
-  });
-  
-function isLoggedIn(req, res, next) {
-    req.user ? next() : res.render('login')
-  }
-
-app.get('/',isLoggedIn ,function (req, res) {
-    res.render('index',{user : req.user});
-});
-
-io.of('/').on('connection', function (client) {
-        console.log('Client connected...');
-        client.on('join', function (data) {
-            console.log(data + ' Join');
-        });
-        client.on('messages',function(data){
-            client.emit('thread',data);
-            client.broadcast.emit('thread',data);
-        });
-    });
-
+io.of("/").on("connection", function (client) {
+	console.log("Client connected...")
+	client.on("join", function (data) {
+		console.log(data + " Join")
+	})
+	client.on("messages", function (data) {
+		client.emit("thread", data)
+		client.broadcast.emit("thread", data)
+	})
+})
 
 server.listen(port)
